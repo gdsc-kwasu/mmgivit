c14777185c6a9838b59d428f6116d9c053def522
c-blaq <babayejuadesina@gmail.com>
2022-05-10 16:14:01 +0100

    responsiveness: subscribe to newsletter

--- src/containers/Home/style/NewsLetter.styled.js
+++ src/containers/Home/style/NewsLetter.styled.js
@@ -4,19 +4,21 @@ export const NewsLetterContainer = styled.section`
   border: 1px solid #dfdfd7;
   padding: 3rem 0;
   background: #f6fff0;
+  margin: 2.5rem 0;
 
   .subscribe {
     display: flex;
     flex-wrap: wrap;
     align-items: center;
+    justify-content: space-between;
     width: 90%;
     margin: auto;
   }
 
   .header-text-wrapper {
-    // font-size: 1.125rem;
     color: #121212;
     text-align: center;
+    width: 100%;
   }
   span {
     display: block;
@@ -34,7 +36,6 @@ export const NewsLetterContainer = styled.section`
   .subscribe-input-email {
     border: 1px solid #c1c1ba;
     margin: 3rem 0;
-    // width: 100%;
     outline: none;
     flex: 1;
     padding: 1rem;
@@ -48,5 +49,34 @@ export const NewsLetterContainer = styled.section`
     text-transform: uppercase;
     width: 50%;
     margin: 0 auto;
+  }
+
+  @media (min-width: 500px) {
+    .subscribe-form {
+      margin-top: 1.5rem;
+    }
+
+    .subscribe-input-email {
+      width: 60%;
+      margin: 0 1rem;
+    }
+
+    .subscribe-btn-submit {
+      width: 40%;
+      max-width: 200px;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    .header-text-wrapper {
+      text-align: left;
+      width: 40%;
+      // max-width: 282px;
+    }
+
+    .subscribe-form {
+      margin: 0;
+      width: 60%;
+    }
   }
 `;
