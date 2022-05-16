2e242368e18b6733719f68ea9419c4a21d66fab5
c-blaq <babayejuadesina@gmail.com>
2022-05-16 21:43:27 +0100

    refactor: header's height

--- src/layout/PageLayout/style/Header.styled.js
+++ src/layout/PageLayout/style/Header.styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   background: url("/images/header-bg-2.png") center/cover no-repeat;
-  height: 400px;
+  height: 550px;
   color: #f6fff0;
 
   @media (min-width: 765px) {
