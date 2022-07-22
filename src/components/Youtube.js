762d0dca4081085305a602be438ada24ca96e8f1
kaf-lamed-beyt <belac335@gmail.com>
2022-07-22 15:03:36 +0100

    fix: react camelCase error of native html element

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -10,7 +10,7 @@ export default function Youtube({ video }) {
       title="YouTube video player"
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullscreen
+      allowFullScreen
     ></iframe>
   );
 }
