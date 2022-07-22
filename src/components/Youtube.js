5b5a7b55b2ef695409124abeb2db52aa4d099977
kaf-lamed-beyt <belac335@gmail.com>
2022-07-22 14:53:00 +0100

    fix: react camelCase error of native html elements

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -8,9 +8,9 @@ export default function Youtube({ video }) {
       height="315"
       src={`https://www.youtube.com/embed/${video}`}
       title="YouTube video player"
-      frameborder="0"
+      frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
+      allowFullscreen
     ></iframe>
   );
 }
